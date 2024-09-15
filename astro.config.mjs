6e4f2ffbb839005ef71f54bf0471a127682cab3c
Pierre Blarre <pierre.blarre@gmail.com>
2024-09-15 23:16:50 +0200

    Initial setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,23 +4,58 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://erralb.github.io/ia',
+	base: '/ia/',
+
+	// site: 'https://erralb.com/ia',
+	// base: '/ia/',
+
+	// site: import.meta.env.SITE,
+	// base: import.meta.env.BASE,
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Intelligence Artificelle',
+			logo: {
+			  src: './src/assets/logo.svg',
+			},
+			favicon: './src/assets/logo.svg',
+			customCss: [
+				'./src/style.css',
+			],
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/erralb/ia'
+			},
+			components: {
+			  ThemeSelect: './src/components/ThemeSelect.astro',
 			},
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+					label: '1. Introduction',
+					slug: '01-introduction',
 				},
+				// {
+				// 	label: '1. Introduction',
+				// 	collapsed: true,
+				// 	autogenerate: {
+				// 		directory: '01-concepts',
+				// 	}
+				// 	// collapsed: true,
+				// 	// items: [
+				// 	// 	{
+				// 	// 		label: '1. Définitions et historique',
+				// 	// 		collapsed: true,
+				// 	// 		autogenerate: {
+				// 	// 			directory: '01-concepts',
+				// 	// 		}
+				// 	// 	},
+				// 	// ]
+				// },
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: '2. Recherche Heuristique',
+					collapsed: true,
+					autogenerate: {
+						directory: '02-recherche',
+					}
 				},
 			],
 		}),
